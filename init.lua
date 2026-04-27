baefc92390e5854cdae09aeb188785606aa2e3ac
Drew Camron <dcamron@ucar.edu>
2026-04-27 11:03:16 -0600

    picker include hidden files

--- init.lua
+++ init.lua
@@ -774,7 +774,7 @@ require('lazy').setup({
       input = { enabled = true },
       lazygit = { enabled = true },
       notifier = { enabled = true },
-      picker = { enabled = true },
+      picker = { enabled = true, sources = { files = { hidden = true }, smart = { hidden = true } } },
       quickfile = { enabled = true },
       scope = { enabled = true },
       words = { enabled = true },
