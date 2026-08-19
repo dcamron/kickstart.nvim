e7376fc1db33ea33908714cca5f20144d8957cde
Drew Camron <dcamron@ucar.edu>
2026-08-18 20:23:39 -0500

    add rust to treesitter and format

--- init.lua
+++ init.lua
@@ -855,7 +855,21 @@ require('lazy').setup({
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
       -- ensure basic parser are installed
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml' }
+      local parsers = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'rust',
+        'vim',
+        'vimdoc',
+        'yaml'
+      }
       require('nvim-treesitter').install(parsers)
 
       ---@param buf integer
